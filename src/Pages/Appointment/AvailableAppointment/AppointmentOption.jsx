5bebaf3400f1588ca7b0dfe240a4696011ee1884
f-rahim <freelancerrahim26@gmail.com>
2023-06-24 10:54:09 +0600

    Added Appointment available options

---
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+const AppointmentOption = ({ option }) => {
+  const { name, slots } = option;
+  return (
+    <div
+      className="cardbg-base-100 shadow-xl rounded-lg"
+      style={{ boxShadow: "3px 4px 10px 2px rgba(0, 0, 0, 0.05)" }}
+    >
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{slots?.length > 0 && slots[0]}</p>
+        <p>{slots.length} spaces available</p>
+        <div className="card-actions mt-4">
+          <button
+            className="btn site-gradient text-white border-0"
+            disabled={slots.length === 0}
+          >
+            Book Appointment
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentOption;
--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.jsx
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.jsx
@@ -1,7 +1,17 @@
 /* eslint-disable react/prop-types */
 import format from "date-fns/format";
+import { useEffect, useState } from "react";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointment = ({ selectedDate }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState();
+
+  useEffect(() => {
+    fetch(`appointmentOptions.json`)
+      .then((res) => res.json())
+      .then((data) => setAppointmentOptions(data));
+  }, []);
+
   return (
     <section className="available-appointment py-14 md:py-20">
       <div className="site-container">
@@ -11,7 +21,16 @@ const AvailableAppointment = ({ selectedDate }) => {
           </h2>
         </div>
 
-        <div className="booking-row"></div>
+        <div className="booking-row mt-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {appointmentOptions?.map((option) => (
+              <AppointmentOption
+                key={option._id}
+                option={option}
+              ></AppointmentOption>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
