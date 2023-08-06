bb075062f2ac965a1bc5405591f4ca3c321b2edb
f-rahim <freelancerrahim26@gmail.com>
2023-08-06 12:33:56 +0600

    Remove AuthContex from AddDoctor

--- src/Pages/Dashboard/AddDoctor/AddDoctor.jsx
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from "react-query";
-import { useAuthContex } from "../../../Contex/AuthProvider";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { FaRegImage } from "react-icons/fa";
