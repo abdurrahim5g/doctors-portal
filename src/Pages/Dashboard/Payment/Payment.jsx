e5ae197ba34a57595b4b81fdc60b4ddad3ccd77d
f-rahim <freelancerrahim26@gmail.com>
2023-08-08 11:03:22 +0600

    Show Pashent name in the UI

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -3,12 +3,17 @@ import { useLoaderData } from "react-router-dom";
 const Payment = () => {
   const data = useLoaderData();
   console.log(data);
+  const { pashent, price, tritmentName } = data;
 
   return (
     <div className="payment-page">
-      <h2 className="text-2xl font-semibold mb-3">Payment</h2>
+      <h2 className="text-2xl font-semibold mb-3">Payment for {pashent}</h2>
 
-      <div className="dashboard-content bg-white py-6 px-5 rounded-lg shadow-lg"></div>
+      <div className="dashboard-content bg-white py-6 px-5 rounded-lg shadow-lg">
+        <p>
+          Please pay <strong>${price}</strong> for booking {tritmentName}
+        </p>
+      </div>
     </div>
   );
 };
